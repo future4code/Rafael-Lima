1c59a2f2792fec82b0ad555780f77ec809c37fd2
Rafael-Lima <devrafaelflores@gmail.com>
2021-08-12 16:50:33 -0300

    atividade de funcoes incompleta

---
+++ funcoes/index.js
@@ -0,0 +1,21 @@
+// EXERCÍCIO DE LEITURA
+
+//1.
+//a. 10 e 50.
+//b. não aparece nada.
+
+// //2.
+//i. a saída será: true
+// ii. a saída será : true
+// iii. a saída será :true
+
+
+//------EXERCÍCIO DE ESCRITA
+
+//1.
+
+const funcaoEscrita1 = function funcaoNaoFazNada(){
+    const textoPredefinido = "Eu sou Caio, tenho 23 anos, moro em São Paulo e sou estudante."
+}
+
+console.log(funcaoEscrita1)
